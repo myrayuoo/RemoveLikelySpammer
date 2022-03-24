dfe89df86bff1c8e60c2874bfd8d5f2b12af9d1a
verlox <verlox@riseup.net>
2022-03-23 21:58:11 -0400

    fix previous not defined errors, add more comments to explain code

--- RemoveLikelySpammer.plugin.js
+++ RemoveLikelySpammer.plugin.js
@@ -20,38 +20,51 @@ module.exports = class RemoveLikelySpammer {
   start() {}
   stop() {}
 
+  // This will catch all HTML mutations, good for catching new messages
   observer(changes) {
     // Iterate through the added nodes
     for (let x =0;x < changes.addedNodes.length;x++) {
+      // Get a node that was added
       let node = changes.addedNodes[x];
       
-      if (node.className?.includes('chatContent'))
-        this.expandAll(node.firstChild.firstChild.firstChild.firstChild.children);
-      else
-        this.expand(node);
+      // If it contains chat content, that means that it reregistered the chat box, indicating a channel switch
+      // otherwise it is just a regular node
+      if (node.className?.includes)
+        if (node.className?.includes('chatContent'))
+          this.expandAll(node.firstChild.firstChild.firstChild.firstChild.children);
+        else
+          this.expand(node);
     }
   }
 
+  // Expand a node in the chatbox
   expand(node) {
     try {
       // Ignore anything that is not grouped message
-      if (!node.className?.includes('groupStart')) return;
-  
+      if (!node.className?.includes('groupStart') && !node.className?.includes('messageList')) return;
+
       // Make sure it's a likely spammer group and not some other sys message
       let desc = node.firstChild?.firstChild?.firstChild?.lastChild;
       if (!desc?.firstChild?.innerText?.includes('likely spammer')) return;
+
+      // Simulate a click to the 'show message' button
       desc.firstChild.lastChild.click();
 
+      // Get all the messages in the group that gets displayed
       let messages = [...node.children].filter(child => child.tagName == 'LI');
 
+      // Get the raw HTML for the messages only (not including the message group part)
       let raw = '';
       for (let x = 0;x < messages.length;x++)
         raw += messages[x].innerHTML;
 
+      // Set the actual node to the 'show messages' content
+      // This actually removes the 'show message' group above
       node.innerHTML = raw;
     } catch (ex) {console.error(ex);}
   }
 
+  // Expand all nodes in the chat box
   expandAll(nodes) {
     for (let x = 0;x < nodes.length;x++)
       this.expand(nodes[x]);
